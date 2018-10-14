7852096e080ef578e665298ec1cddbdfff9f6fc0
Jorge Fernandez-de-Cossio-Diaz <j.cossio.diaz@gmail.com>
2018-10-14 10:23:10 -0400

    minor

--- README.md
+++ README.md
@@ -3,11 +3,10 @@
 [![Build Status](https://travis-ci.org/cossio/LambertWExp.jl.svg?branch=master)](https://travis-ci.org/cossio/LambertWExp.jl)
 [![Coverage Status](https://coveralls.io/repos/github/cossio/LambertWExp.jl/badge.svg?branch=master)](https://coveralls.io/github/cossio/LambertWExp.jl?branch=master)
 
-Computes lambertw(exp(x)), without intermediate overflows.
+Computes `W(e^x)` for real x and the principal branch of W, avoiding intermediate overflow.
 
 Based on https://github.com/jlapeyre/LambertW.jl.
 
-
-# TODO
+## TODO
 
 Consider implementing Fritsch iteration, which should be faster (Veberic 2012).
--- test/wexp.jl
+++ test/wexp.jl
@@ -9,9 +9,26 @@ lambexpinv(u) = log(u) + u
     @test iszero(lambertwexp(-Inf))
     @test lambertwexp(Inf) == Inf
     @test isnan(lambertwexp(NaN))
+    @test isnan(lambertwexp(Float16(NaN)))
 
-    @inferred lambertwexp(1)
-    @inferred lambertwexp(-1)
+    for x in -100 : 100
+        @inferred lambertwexp(x)
+        @inferred lambertwexp(Float16(x))
+        @inferred lambertwexp(Float64(x))
+    end
+
+    for x in [NaN, Inf, -Inf]
+        @inferred lambertwexp(x)
+        @inferred lambertwexp(Float16(x))
+    end
+end
+
+
+@testset "lambertwexp numerical value" begin
+    for x = -100 : 1 : 10000
+        @inferred lambertwexp(x)
+        @test lambexpinv(lambertwexp(x)) ≈ x  atol = 1e-15
+    end
 
     for x in [1e4, 1e5, 1e6]
         @test lambexpinv(lambertwexp(x)) ≈ x
@@ -19,16 +36,8 @@ lambexpinv(u) = log(u) + u
 end
 
 
-@testset "lambertwexp numerical value" begin
-    for x = -100 : 1 : 1000
-        @inferred lambertwexp(x)
-        @test lambexpinv(lambertwexp(x)) ≈ x  atol = 1e-15
-    end
-end
-
-
 @testset "lambertwexp derivatives" begin
-    for w = 1 : 10
+    for w = 1 : 1000
         x = w + log(w)
         @test lambertwexp_d1_from_W(w) ≈ ForwardDiff.derivative(lambertwexp, x)
         @test lambertwexp_d2_from_W(w) ≈ ForwardDiff.derivative(lambertwexp_d1_from_W ∘ lambertwexp, x)
